3c26fd597cdf804f03a245442fdd4f0989414fb5
Usuario <gemachvaez2204@gmail.com>
2020-10-22 13:19:23 +0200

    residencia y sistema

--- prueba.java
+++ prueba.java
@@ -1,6 +1,7 @@
  public claas Prueba{
 	public static void main(String args[]){
         System.out.println("gema chavez");
-	System.out.println("quito - 2000")
+	System.out.println("quito - 2000");
+	System.out.println("carretera asturias - windows 10")
     }
 }
