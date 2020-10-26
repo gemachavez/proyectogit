e4407ea145915357457711392f228caec4053869
Usuario <gemachvaez2204@gmail.com>
2020-10-26 09:50:21 +0100

    nombre completo

--- prueba.java
+++ prueba.java
@@ -2,6 +2,7 @@
 	public static void main(String args[]){
         System.out.println("gema chavez");
 	System.out.println("quito - 2000");
-	System.out.println("carretera asturias - windows 10")
+	System.out.println("carretera asturias - windows 10");
+	System.out.println("gema chavez valdez");
     }
 }
